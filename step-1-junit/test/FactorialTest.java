47aee1d500e0ac94a38f54bde187f55bfaa4b379
Edward McCue <edward.mccue@adacollege.org.uk>
2017-06-07 12:09:00 +0100

    added third test

--- step-1-junit/test/FactorialTest.java
+++ step-1-junit/test/FactorialTest.java
@@ -26,4 +26,12 @@ public class FactorialTest {
 
         assertEquals(expectedResult, factorial.calculate(testStartValue));
     }
+
+    @Test
+    public void calculate_5() {
+        int testStartValue = 5;
+        int expectedResult = 120;
+
+        assertEquals(expectedResult, factorial.calculate(testStartValue));
+    }
 }
