8f0f30c0108e925f5b0fb2ef3051837ddb68ea38
Edward McCue <edward.mccue@adacollege.org.uk>
2017-06-12 09:22:36 +0100

    Files for steps 2 and 3

---
+++ step-3-maven-fibonacci/src/test/java/com/mattcalthrop/examples/factorial/FactorialTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FactorialTest {
+    private Factorial factorial;
+
+    @Before
+    public void setUp() {
+        factorial = new Factorial();
+    }
+
+    @Test
+    public void calculate_0() {
+        int testStartValue = 0;
+        int expectedResult = 0;
+
+        assertEquals(expectedResult, factorial.calculate(testStartValue));
+    }
+
+    @Test
+    public void calculate_1() {
+        int testStartValue = 1;
+        int expectedResult = 1;
+
+        assertEquals(expectedResult, factorial.calculate(testStartValue));
+    }
+}
